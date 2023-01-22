dd09ff0dbe3ecf10a745663beee2a49bb5ee47ef
putusujane <blogibagi@gmail.com>
2023-01-23 02:45:37 +0700

    Selesai penambahan fitur responsive mobile

--- Scripts/script.js
+++ Scripts/script.js
@@ -24,14 +24,15 @@ const gelap = (id, nilai) => {
     document.body.style.backgroundColor = "unset";
     document.getElementById("header").style.backgroundColor = "rgba(255, 255, 255, 0.9)";
     
-    const nodeList = document.querySelectorAll("#navigasi-atas a, #navigasi-atas i, a, h1, h2, h3, p, figcaption, footer i");
+    const nodeList = document.querySelectorAll("#navigasi-atas a, #navigasi-atas i, #navigasi-mobile i, #bungkus-menuNM i, a, h1, h2, h3, p, figcaption, footer i");
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].style.color = "#212121";
     }
 
     document.getElementById("footer").style.background = "#f5f5f5";
+    document.getElementById("bungkus-menuNM").style.backgroundColor = "rgba(255, 255, 255, 0.9)";
 }
-
+// ============================================================================
 const terang = (id, nilai) => {
     document.getElementById("terang").style.display = "none";
 
@@ -40,12 +41,15 @@ const terang = (id, nilai) => {
     document.body.style.backgroundColor = "#1e1e1e";
     document.getElementById("header").style.backgroundColor = "rgba(30, 30, 30, 0.9)";
     
-    const nodeList = document.querySelectorAll("#navigasi-atas a, #navigasi-atas i, a, h1, h2, h3, p, figcaption, footer i");
+    const nodeList = document.querySelectorAll("#navigasi-atas a, #navigasi-atas i, #navigasi-mobile i, #bungkus-menuNM i, a, h1, h2, h3, p, figcaption, footer i");
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].style.color = "#f5f5f5";
     }
 
     document.getElementById("footer").style.backgroundColor = "rgba(48, 48, 48, 0.5)";
+
+    // warna background menu mobile
+    document.getElementById("bungkus-menuNM").style.backgroundColor = "rgba(30, 30, 30, 0.9)";
 }
 
 
